01b6a8349611dec9ec5ae1e70e096241966a0933
Ricardo Cañuelo Navarro <ricardo.canuelo@gmail.com>
2011-07-18 13:17:53 +0200

    Starting CFS implementation

---
+++ platform/gam-sdk/dev/xmem.c
@@ -0,0 +1,54 @@
+/*
+ * Device driver for the ST M25P40 external memory
+ */
+
+#include "contiki.h"
+#include "dev/xmem.h"
+#include "mc1322x.h"
+#include <stdio.h>
+
+nvmType_t type = 0;
+
+void xmem_init(void)
+{
+	nvmErr_t err;
+	err = nvm_detect(gNvmExternalInterface_c, &type);
+}
+
+int xmem_pread(void *buf, int nbytes, unsigned long offset)
+{
+	nvmErr_t err;
+	err = nvm_read(gNvmExternalInterface_c, type, buf, offset, nbytes);
+	if (err == gNvmErrNoError)
+		return nbytes;
+	else
+		return 0;
+}
+
+int xmem_pwrite(const void *buf, int nbytes, unsigned long offset)
+{
+	nvmErr_t err;
+	err = nvm_write(gNvmExternalInterface_c, type, buf, offset, nbytes);
+	if (err == gNvmErrNoError)
+		return nbytes;
+	else
+		return 0;
+}
+
+int xmem_erase(long nbytes, unsigned long offset)
+{
+	nvmErr_t err;
+	unsigned long end = nbytes + offset;
+	if (nbytes % 65536 != 0) {
+		printf("xmem_erase: bad size\n");
+		return -1;
+	}
+	if (offset % 65536 != 0) {
+		printf("xmem_erase: bad offset\n");
+		return -1;
+	}
+	while (offset < end) {
+		err = nvm_erase(gNvmExternalInterface_c, type, offset / 65536);
+		offset += 65536;
+	}
+}
